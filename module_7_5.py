271640616f59146a176542e489a0afe49252bf91
Ldar72 <l-dar@mail.ru>
2024-10-16 15:07:37 +0300

    Add files via upload

---
+++ module_7_5.py
@@ -0,0 +1,14 @@
+import os
+from time import localtime, strftime
+
+directory = '.'
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = strftime('%d.%m.%Y %H:%M', localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, '
+              f'Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
